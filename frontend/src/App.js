0514a1c573e0da7215bdbc686e762e9ccbb52a37
chrisAlide3 <christian.bracher@gmail.com>
2023-08-10 08:41:22 +0200

    Moved DarkTheme and cssBaseLine from index to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import Home from "./pages/Home/Home";
 import { Container } from "@mui/material";
 import Register from "./pages/auth/Register";
@@ -6,19 +8,28 @@ import Login from "./pages/auth/Login";
 import Forgot from "./pages/auth/Forgot";
 import Reset from "./pages/auth/Reset";
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 function App() {
   return (
-    <Container sx={{ backgroundColor: "darkblue", height: "100vh" }}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/forgot" element={<Forgot />} />
-          <Route path="/resetpassword/:resetToken" element={<Reset />} />
-        </Routes>
-      </BrowserRouter>
-    </Container>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container sx={{ backgroundColor: "darkblue", height: "100vh" }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/forgot" element={<Forgot />} />
+            <Route path="/resetpassword/:resetToken" element={<Reset />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
+    </ThemeProvider>
   );
 }
 
